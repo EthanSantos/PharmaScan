40bec416188ea57e62b3d98980a70c65104262e1
EthanSantos <ethanangelo61@gmail.com>
2025-01-25 13:03:29 -0800

    switched to Flask

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/hello")
+      .get("http://127.0.0.1:5000/hello")
       .then((response) => {
         setData(response.data);
         setLoading(false);
