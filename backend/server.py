0ee3d62ba7fb86ffb5c75003fde2fd31da62172c
EthanSantos <ethanangelo61@gmail.com>
2025-01-25 13:53:39 -0800

    / endpoint

--- backend/server.py
+++ backend/server.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  
 
-@app.route("/hello")
-def hello():
+@app.route("/")
+def test():
     return {"message": "Hello World!"}
 
 if __name__ == "__main__":
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:5000/hello")
+      .get("http://127.0.0.1:5000")
       .then((response) => {
         setData(response.data);
         setLoading(false);
