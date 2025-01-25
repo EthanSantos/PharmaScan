af0d098f8e1f442d1a12825c9bf832dd96b0f40e
Benson Manzano <bensonfmanzano@gmail.com>
2025-01-25 14:20:18 -0800

    Added nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,32 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route, 
+  Link, 
+  Navigate,
+  useParams,
+  Outlet 
+} from 'react-router-dom';
 import "./App.css"
+import Example from './pages/example'
+import CameraApp from './pages/cameraApp'
 
-const App = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:5000")
-      .then((response) => {
-        setData(response.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
+function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="p-8 bg-white rounded shadow text-center">
-        <pre className="bg-gray-200 p-4 rounded mt-2 text-gray-800">
-          {data.message}
-        </pre>
-      </div>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Example</Link>
+        <Link to="/camera">Camera App</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Example />} />
+        <Route path="/camera" element={<CameraApp />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
